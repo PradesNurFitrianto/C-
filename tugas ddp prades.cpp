e657eb8b8b26c1ea5be04518443e8505cee17392
PradesNurFitrianto <150000015+PradesNurFitrianto@users.noreply.github.com>
2023-11-09 18:35:40 +0700

    Add files via upload

---
+++ tugas ddp prades.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i,j; 
+  int n = 3;
+  cout << "Nama   : Prades Nur Fitrianto" << endl;
+  cout << "NIM    : 2300018035" << endl;
+  cout << "Kelas  : A" << endl;
+  cout << "==========================" << endl;
+  cout << "****************" << endl;
+  for (i=3; i<=n; i++) { 
+    for (j=1; j<=i; j++) { 
+
+  cout << "*    *    *    *" << endl;
+  cout << "*    *    *    *" << endl;
+  cout << "****************" << endl;
+
+}
+
+ } 
+
+   return 0;
+}
+
